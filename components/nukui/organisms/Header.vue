9342eabaafc4ba933e308d3699ab9d5bf2e461a0
codearts-nukui <y-nukui@codearts.co.jp>
2022-11-08 10:56:05 +0900

    feat: Headerの作成

---
+++ components/nukui/organisms/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app bottom>
+      <MenuList />
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer">I</v-app-bar-nav-icon>
+      <v-toolbar-title>App</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import MenuList from '../molecules/MenuList.vue'
+
+export default defineComponent({
+  name: 'HeaderComponent',
+  components: {
+    MenuList,
+  },
+  setup() {
+    const drawer: Boolean = ref(null)
+    return { drawer }
+  },
+})
+</script>
