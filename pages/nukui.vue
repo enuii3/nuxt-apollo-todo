9f66ba0522e4e7ea275923bfa3f24c88166edaba
codearts-nukui <y-nukui@codearts.co.jp>
2022-11-08 12:05:39 +0900

    fix: nukuiページの修正

--- pages/nukui.vue
+++ pages/nukui.vue
@@ -1,34 +1,14 @@
 <template>
-  <v-app id="inspire">
-    <Header />
-
-    <v-main>
-      <Home />
-      <router-view />
-    </v-main>
-  </v-app>
+  <Home />
 </template>
 
 <script lang="ts">
 import Home from '../components/nukui/organisms/Home.vue'
 
-export interface Item {
-  title: string
-  icon: string
-  to: string
-}
-
 export default defineComponent({
-  name: 'IndexComponent',
+  name: 'HomeComponent',
   components: {
     Home,
-  },
-  setup() {
-    const items = ref([
-      { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
-      { title: 'About', icon: 'mdi-help-box', to: '/about' },
-    ])
-    return { items }
   },
 })
 </script>
