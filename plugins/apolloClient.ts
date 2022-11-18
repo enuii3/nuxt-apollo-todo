32fd4a9ea9aa1fdb22ed4cd332392b0807c1342a
codearts-nukui <y-nukui@codearts.co.jp>
2022-11-18 17:13:48 +0900

    chore: エンドポイント変更

--- plugins/apolloClient.ts
+++ plugins/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 
 export default defineNuxtPlugin(() => {
   const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql',
+    uri: 'http://localhost:4000',
     cache: new InMemoryCache(),
   })
 
